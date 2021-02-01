a0b9ae818154d65357cb62a72c89d2726d488d47
mrChinchill <dartinor2319@gmail.com>
2021-02-02 02:54:25 +0300

    Refactor, add readme

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import requests
 from requests.exceptions import ConnectionError
@@ -9,24 +8,42 @@ from requests.exceptions import HTTPError
 URL = 'http://wttr.in'
 
 
-def get_weather(city):
-    url = '{}/{}'.format(URL, city)
+def get_weather(city, lang='en', unit='u'):
     payload = {
-        'lang': 'ru',
-        'm': ''
+        'n': '',
+        'T': '',
+        'q': '',
+        unit: '',
+        'lang': lang
     }
-    try:
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-    except (ConnectionError, HTTPError):
-        print('Не удалось получитьь погоду в "{}"'.format(city))
-    else:
-        print(response.text)
+
+    url = '{}/{}'.format(URL, city)
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+
+    return response.text
 
 
 def main():
-    for city in ['Лондон', 'Шереметьево', 'Череповец']:
-        get_weather(city)
+    cities = [
+        {'name': 'London', 'lang': 'en', 'unit': 'u'},
+        {'name': 'Шереметьево', 'lang': 'ru', 'unit': 'm'},
+        {'name': 'Череповец', 'lang': 'ru', 'unit': 'm'}
+    ]
+
+    for city in cities:
+        try:
+            result = get_weather(city['name'], lang=city['lang'], unit=city['unit'])
+
+        except (ConnectionError, HTTPError):
+            if city['lang'] == 'ru':
+                error_message = 'Не удалось получить информацию о погоде в "{}"'
+            else:
+                error_message = 'Couldn\'t get weather info for "{}"'
+            print(error_message.format(city['name']))
+
+        else:
+            print(result)
 
 
 if __name__ == '__main__':
